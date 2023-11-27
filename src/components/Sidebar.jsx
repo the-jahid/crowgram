c11d71aa84b3f62eb4fa10aa95c7bb99e814f9f6
the-jahid <aenor007@gmail.com>
2023-11-26 20:58:19 -0600

    'color_fixed'

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ const SideBar = ({opensideBar, setSideBar}) => {
 
   
   return (
-    <div className={` px-4  z-20 absolute top-0 ${opensideBar ? 'left-0' : '-left-2/3'} w-2/3 h-screen bg-black transition-all duration-5000 ease-in-out`}>
+    <div className={` px-4  z-20 absolute top-0 ${opensideBar ? 'left-0' : '-left-2/3'} w-2/3 h-screen bg-custom-black transition-all duration-5000 ease-in-out`}>
     <div className="pl-7 mt-4" >
         <p onClick={() => setSideBar(false)} className="text-xl cursor-pointer">x</p>
     </div>
